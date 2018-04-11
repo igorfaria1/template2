f8eb0d013cc93ea0859310a04662019a4b8d1c7e
Igor <igor_faria_jf@hotmail.com>
2018-04-10 21:32:28 -0300

    Commit Inicial: Organização de Projeto e criação do Header.php

---
+++ header.php
@@ -0,0 +1,26 @@
+<header id="header">
+        <div class="header-image">
+        </div>
+        <div class="bg-red">
+        </div>
+        <div class="content-header row align-items-center">
+            <div class="col-12 col-md-6 col-sm-6 text-right">
+               <img src="img/user.jpg" class="img-profile img-responsive" alt="">
+            </div>
+            
+            <div class="col-12 col-md-6 col-sm-6">
+               <div class="row header-info">
+                    <div class="col-md-12 col-sm-12 col-12">
+                        <h1 class="name">IGOR FARIA</h1>
+                    </div>
+                    
+                    <div class="col-md-12 col-sm-12 col-12">
+                        <p class="profession">Desenvolvedor Web</p>
+                    </div>
+               </div>
+            </div>
+        
+        </div>
+        
+          
+</header>
---
+++ index.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="pt-br">
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.9/css/all.css">
+        <link rel="stylesheet" href="css/template2.css">
+        
+        <link href="https://fonts.googleapis.com/css?family=Exo:300,400,600,700,900" rel="stylesheet">
+        
+        <title>Igor Faria - Desenvolvedor Web</title>
+    </head>
+    <body>
+        
+        <div class="container-fluid">
+                <!--HEADER-->
+                    <?php include_once('header.php') ?>
+                <!--END HEADER-->
+        </div>
+        
+        
+    
+    
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"></script>
+    </body>
+</html>
